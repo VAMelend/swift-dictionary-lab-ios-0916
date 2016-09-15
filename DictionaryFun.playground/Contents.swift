a34d9a663c078bd23215cd36d04fb8eb2caa871e
Victoria Melendez <Victoria@Victorias-MacBook-Pro.local>
2016-09-15 11:03:35 -0400

    Done

--- DictionaryFun.playground/Contents.swift
+++ DictionaryFun.playground/Contents.swift
@@ -15,7 +15,13 @@
 // write your code here
 
 
-
+var states = [
+"New York": "Albany",
+"Ohio": "Columbus",
+"Florida": "Tallahassee",
+"Georgia": "Atlanta",
+"Kentucky": "Frankfort"
+]
 
 
 
@@ -24,7 +30,7 @@
  ### 2. What is the type of the dictionary you created in Question 1?
  */
 
-
+//key value
 
 
 
@@ -37,7 +43,8 @@
 // write your code here
 
 
-
+var capital = states["Florida"]
+print (capital)
 
 
 
@@ -47,6 +54,8 @@
  ### 4. Unwrap `capital` and print out the message "The capital of Florida is <Capital>" to the console.
  */
 // write your code here
+
+print("The capital of Florida is \(capital)")
 
 
 
@@ -61,7 +70,7 @@
 // write your code here
 
 
-
+var anotherCapital = states["Pennsylvania"]
 
 
 
@@ -72,9 +81,11 @@
  */
 // write your code here
 
-
-
-
+if let anotherCapital = states["Pennsylvania"] {
+    print("The capital of PA is  \(anotherCapital).")
+} else {
+    print("I don't know Pennsylvania's capital.")
+}
 
 
 
@@ -85,6 +96,9 @@
 // write your code here
 
 
+states["Pennsylvania"] = "Harrisburg"
+
+print(states)
 
 
 
@@ -95,6 +109,8 @@
  ### 8. Retrieve Pennsylvania's capital from your dictionary. Unwrap it and print the message "Pennsylvania's capital is <Capital>" to the console.
  */
 // write your code here
+
+print("Pennsylvania's capital is \(states["Pennsylvania"]!)")
 
 
 
@@ -107,6 +123,8 @@
  ### 9. We don't really care about Pennsylvania's capital. Delete it from the dictionary. Print your dictionary to the console to ensure it's gone.
  */
 // write your code here
+states["Pennsylvania"] = nil
+print(states)
 
 
 
@@ -122,7 +140,7 @@ let state = "New York"
 // write your code here
 
 
-
+print("The capital of \(state) is \(states["New York"]!).")
 
 
 
@@ -141,6 +159,12 @@ let state = "New York"
 // write your code here
 
 
+var bands = [
+"Nirvana": ["Kurt Cobain", "Krist Novoselic", "Dave Grohl"],
+"The Beatles": ["John Lennon", "George Harrison", "Paul McCartney", "Ringo Starr"],
+"The Breeders": ["Kim Deal", "Kelley Deal", "Josephine Wiggs", "Jim Macpherson"],
+"Pixies": ["Frank Black", "Joey Santiage", "Kim Deal", "David Lovering"]
+]
 
 
 
@@ -152,7 +176,7 @@ let state = "New York"
 /*: question12
  ### 12. What is the type of the dictionary you created in Question 11?
  */
-
+//array dictionary
 
 
 
@@ -165,6 +189,9 @@ let state = "New York"
  */
 let bandName = "Pixies"
 // write your code here
+print(bands["Pixies"]!)
+
+
 
 
 
